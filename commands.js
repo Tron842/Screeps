2709e1797cf86f5e4ca7670747a939fa8ed41763
tron842 <brodyatto@gmail.com>
2017-06-27 18:08:36 -0400

    Update file structure to work with grunt

--- commands.js
+++
@@ -1,8 +0,0 @@
-Spawn harvester
-Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'})
-
-Spawn Builder
-Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'})
-
-Spawn Upgrader
-Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'})
